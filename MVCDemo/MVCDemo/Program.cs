c8280cd7ff7161f4b618879b838d5218c2f02229
konstant11 <41756988+konstant11@users.noreply.github.com>
2019-09-18 10:50:18 -0700

    .

---
+++ MVCDemo/MVCDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace MVCDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var env = hostingContext.HostingEnvironment;
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                      
+                config.AddEnvironmentVariables();
+            })
+            .UseUrls("http://localhost:5000", "http://0.0.0.0:5000", "https://localhost:5001")
+                .UseStartup<Startup>();
+    }
+}
